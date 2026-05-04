443df5f38820660d0d47c0fd24b486ffc9e85cda
unknown <ilivic@yandex.ru>
2026-05-04 11:58:05 +0300

    3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,41 +9,65 @@ namespace ConsoleApp2
 {
     internal class Program
     {
+        /// <summary>
+        /// Приватная переменная, хранящая информацию об введённом выражений
+        /// </summary>
         private static string _key {  get; set; }
+        /// <summary>
+        /// Приватная переменная, отвечающая за счет времени таймера 
+        /// </summary>
         private static int _t {  get; set; }
+        /// <summary>
+        /// Лист логирования
+        /// </summary>
+        private static List<log> _log {  get; set; }
         static void Main(string[] args)
         {
-           _key = Console.ReadLine();
+            _log = new List<log>();
+            _key = "";
             answer();
         }
+        private static void Logger(string body)
+        {
+            _log.Add(new log()
+            {
+                date = DateTime.Now,
+                rez = body,
+                key = _key,
+            });
+            Console.WriteLine(body);
+        }
+        /// <summary>
+        /// Метод обработки ввода данных
+        /// </summary>
         private static void answer()
         {
             switch (_key.ToLower())
             {
                 case "":
                     {
-                        Console.WriteLine("Всё хорошо");
+                        Logger("Всё хорошо");
                         _key = Console.ReadLine();
                         answer();
                         break;
                     }
                 case "привет":
                     {
-                        Console.WriteLine("Привееет Как ты?)");
+                        Logger("Привееет Как ты?)");
                         _key = Console.ReadLine();
                         answer();
                         break;
                     }
                 case "сколько время":
                     {
-                        Console.WriteLine("Сейчас" + DateTime.Now);
+                        Logger("Сейчас" + DateTime.Now);
                         _key = Console.ReadLine();
                         answer();
                         break;
                     }
                 case "запусти секундомер":
                     {
-                        Console.WriteLine("запустил");
+                        Logger("запустил");
                         Task.Run(Ctimer);
                         _key = Console.ReadLine();
                         answer();
@@ -51,27 +75,51 @@ namespace ConsoleApp2
                     }
                 case "сколько прошло":
                     {
-                        Console.WriteLine(_t.ToString());
+                        Logger(_t.ToString());
                         _key = Console.ReadLine();
                         answer();
                         break;
                     }
                 case "заверши":
                     {
-                        Console.WriteLine("Завершил");
+                        Logger("Завершил");
                         _t = -1;
+                        _key = Console.ReadLine();
+                        answer();
+                        break;
+                    }
+                case "история":
+                    {
+                        Logger("вывожу историю чата");
+                        Task.Run(()=> ShowHist());
                         _key = Console.ReadLine();
                         answer();
                         break;
                     }
                 default:
                     {
+                        Logger("-------------");
                         _key = Console.ReadLine();
                         answer();
                         break;
                     }
             }
         }
+        /// <summary>
+        /// Метод вывода логов
+        /// </summary>
+        private static async void ShowHist()
+        {
+            foreach (var item in _log.ToList())
+            {
+                Console.WriteLine(item.key+" ");
+                Console.Write(item.rez+ " ");
+                Console.Write(item.date+ " ");
+            }
+        }
+        /// <summary>
+        /// Метод имитирующий таймер (асинхронный)
+        /// </summary>
         private static async void Ctimer()
         {
             _t = 0;
@@ -84,6 +132,13 @@ namespace ConsoleApp2
                     break;
                 }
             }
+        }
+
+        private class log
+        {
+            public string key { get; set; } 
+            public string rez { get; set; } 
+            public DateTime date { get; set; } 
         }
 
     }
